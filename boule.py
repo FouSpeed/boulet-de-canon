41fbe1457cc91e930584c73e44f96e8047ecc4eb
FouSpeed <felmythomas@gmail.com>
2023-05-07 09:41:25 +0200

    buger

--- boule.py
+++ boule.py
@@ -4,6 +4,8 @@ import math
 from circle import *
 
 class Boule:
+    center_balls = []
+
     def __init__(self, velocity, angle, surface:Window, radius, circle:Circle, x_decal = 0, y_decal = 0):
         self.radius = radius
         self.surface = surface
@@ -12,11 +14,14 @@ class Boule:
         self.angle = angle
         self.y = surface.window_size[1]//2 + y_decal
         self.y_before = self.y
+        self.position = (self.x, self.y)
         self.velocity = velocity
         self.vel_x = self.velocity * math.cos(math.radians(self.angle))
         self.vel_y = -self.velocity * math.sin(math.radians(self.angle))
         self.sens = "right"
         self.circle = circle
+        self.center_ball = []
+        self.i_center_balls = 0
         
         
         self.variation_speed_y = 0
@@ -26,27 +31,35 @@ class Boule:
 
     def afficher(self):
         self.calculate_new_coordinate()
-        
+        self.position = (self.x, self.y)    
         pygame.draw.circle(self.surface.screen, (255,255,255), (self.x, self.y), self.radius)
         self.x_before = self.x
         self.y_before = self.y
+
     
     def calculate_new_coordinate(self):
 
         delta_time = self.clock.tick_busy_loop(60) / 1000.0
         self.variation_speed_y = self.g * delta_time
         self.vel_y += self.variation_speed_y
+        self.collision_circulaire()
         self.x += self.vel_x
         self.y += self.vel_y
-        self.collision_circulaire()
+        
     
     def collision_circulaire(self):
         distance_center_cercle = math.sqrt((self.circle.x-self.x)**2 + (self.circle.y - self.y)**2)
 
         if distance_center_cercle + self.radius >= self.circle.radius:
-            alpha = (self.x_before-self.x)/(self.y_before-self.y)
+            if self.y - self.y_before != 0:
+                alpha = (self.x_before-self.x)/(self.y_before-self.y)
+                print(alpha)
+            elif (self.x_before>self.x and self.y_before>self.y) or (self.x_before<self.x and self.y_before<self.y):
+                alpha = 1e-7
+            else:
+                alpha = 1e-7
 
-            petit_beta = math.atan2(self.y, self.x) - 0.001
+            petit_beta = math.atan2(self.y, self.x) - 1e-9
             x_near = self.circle.x + self.circle.radius * math.cos(petit_beta)
             y_near = self.circle.y + self.circle.radius * math.sin(petit_beta)
 
@@ -56,4 +69,28 @@ class Boule:
             theta = alpha -  2 * phi
             self.vel_x = math.cos(theta) * 10
             self.vel_y = -math.sin(theta) *10
-        
+    
+    def is_collision_ball(self):
+        for i, position in enumerate(Boule.center_balls):
+            distance_center_ball = math.sqrt((Boule.center_balls[i][0]-self.x)**2 + (Boule.center_balls[i][1] - self.y)**2)
+
+            if distance_center_ball + self.radius >= self.radius and i != self.i_center_balls:
+                return i
+        return -1
+
+    def collision_ball(self, all_balls:list):
+        i_collision = self.is_collision_ball
+
+        if i_collision>=0:
+            alpha = (self.x_before-self.x)/(self.y_before-self.y)
+
+            petit_beta = math.atan2(self.y, self.x) - 1e-0
+            x_near = Boule.center_balls[i_collision][0] + self.radius * math.cos(petit_beta)
+            y_near = Boule.center_balls[i_collision][1] + self.radius * math.sin(petit_beta)
+
+            beta = -math.atan2(self.y - y_near, self.x - x_near)
+
+            phi = alpha - beta
+            theta = alpha -  2 * phi
+            self.vel_x = math.cos(theta) * 10
+            self.vel_y = -math.sin(theta) *10
--- main.py
+++ main.py
@@ -6,13 +6,17 @@ from circle import *
 
 pygame.init()
 boules = []
+center_ball = []
 window = Window()
 circle = Circle(window)
-for i in range(10):
-    boules.append(Boule(10, 5, window, 10,circle, x_decal=i))
+for i in range(1):
+    boules.append(Boule(20, 200, window, 10,circle, x_decal=i))
+    Boule.center_balls.append(boules[i].position)
+    boules[i].i_center_balls = i
 while True:
-    for boule in boules:
+    for j,boule in enumerate(boules):
         boule.afficher()
+        Boule.center_balls[j] = boule.position
     circle.afficher()
 
 
